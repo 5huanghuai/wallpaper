99b599cf5de22f9ea8ad1fc50a272e7d7dc9e0fc
windows.ins <mr.dongsuibian@gmail.com>
2023-04-04 11:40:55 +0800

    init

---
+++ wallpaper/1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# 用于快速重命名全部文件
+i=0; 
+for f in $(ls); do mv -v "$f" $(printf "%06d.png" $i); 
+((i=i+1)); 
+done
+
+echo '## wallpapers' > README.md
+for file in $(ls | grep -v 1.sh | grep -v README); do
+    echo "$file![$file]($file)" >> README.md
+done
