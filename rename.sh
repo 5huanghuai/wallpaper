8f0b2b7996e30612b0ecade2107bdf5e09ef28c2
MyArch.ins <mr.dongsuibian@gmail.com>
2023-04-08 18:36:15 +0800

    add rename.sh and chang file struc

---
+++ rename.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# 用于快速重命名全部文件
+#
+cd ./wallpaper;
+i=0; 
+for f in $(ls); do mv -v "$f" $(printf "%06d.png" $i); 
+((i=i+1)); 
+done
+
+echo '## wallpapers' > ../README.md
+for file in $(ls | grep -v 1.sh | grep -v README); do
+    echo "$file![$file]($file)" >> ../README.md
+done
